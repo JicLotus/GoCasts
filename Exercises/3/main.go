6664ad5586154aa8ba58c956bb8f58704a46fa0f
jiclotus <joseignaciocastelli92@gmail.com>
2019-07-03 17:53:33 -0300

    Adding 3th ex.

--- Exercises/3/main.go
+++ Exercises/3/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
 
+	fn := os.Args[1]
+	f, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	io.Copy(os.Stdout, f)
 }
